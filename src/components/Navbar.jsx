4529f9cdfb8c77d2f7e64d64ed44fb7b4356d29a
jermaine <jblearn2023@gmail.com>
2024-09-08 17:31:46 +0100

    added icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
-import {FaLinkedin} from "react-icons/fa";
+import logo from "../assets/projects/kevinRushLogo.png";
+import {FaLinkedin, FaLinkedinIn} from "react-icons/fa";
 import {FaGithub} from "react-icons/fa";
 import {FaSquareXTwitter} from "react-icons/fa6";
 import {FaInstagram} from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <nav className="bg-blue-900 mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-     
-
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4">
+        <FaLinkedinIn />
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaInstagram />
       </div>
     </nav>
   )
