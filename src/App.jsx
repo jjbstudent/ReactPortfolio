df2cb6b10dd3ebd244b8f72897ba01f55bdda5d2
jermaine <jblearn2023@gmail.com>
2024-09-09 21:27:41 +0100

    update Hero Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/NavBar'
+import Hero from './components/hero'
 
 const App = () => {
   return (
-    <div>
-      <h1>Jermaine Portfolio</h1>
-      <div><Navbar /></div>
+    <div className='overflow-x-hidden text-neutral-300 aniliased selection:bg-cyan-300'>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className='flixed top-0 -z-10 h-full w-full'></div>
+      <div className='container mx-auto px-8'></div>
+      <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2">
+                <div className="flex flex-col items-center lg:items-start">
+                    <h1>Jermaine Bandoo</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Hero;
