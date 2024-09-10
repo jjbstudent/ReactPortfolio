b260813dcb4e705bd051ae5e050567a3081dc91d
jermaine <jblearn2023@gmail.com>
2024-09-10 21:58:08 +0100

    update icon links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,23 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-      <img src={logo} alt="logo" style={{ width: "150px", height: "auto" }} />
+      <img src={logo} alt="logo" style={{ width: "100px", height: "auto" ,marginLeft: "20px"}} />
       </div>
       <div className="m-8 flex items-center justify-center gap-4">
-        <FaLinkedinIn />
-        <FaGithub />
-        <FaSquareXTwitter />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/jermaine-bandoo-bb7b802a5/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedinIn size={40} />
+        </a>
+        <a href="https://github.com/jjbstudent" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={40} />
+        </a>
+        <a href="https://x.com/jblearn2023" target="_blank" rel="noopener noreferrer">
+          <FaSquareXTwitter size={40} />
+        </a>
+        <a href="https://www.instagram.com/alphajazz81/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={40} />
+        </a>
       </div>
+
     </nav>
   )
 };
