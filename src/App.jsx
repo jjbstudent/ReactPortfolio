047fb48df96bdb93ccb0558bf416ab9435539f4e
jermaine <jblearn2023@gmail.com>
2024-09-15 17:24:56 +0100

    update about

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ import Hero from './components/hero'
 const App = () => {
   return (
     <div className='overflow-x-hidden text-neutral-300 aniliased selection:bg-cyan-300'>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      <div className='flixed top-0 -z-10 h-full w-full'></div>
+      
+      <div className='fixed top-0 -z-10 h-full w-full'>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+
+        </div>
+      </div>
       <div className='container mx-auto px-8'></div>
       <Navbar />
       <Hero />
